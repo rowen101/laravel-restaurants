540896e44dc218a7bb376d980f53dfef95fa406e
rowen101 <rgrowengonzales66@gmail.com>
2022-09-01 14:57:30 +0800

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Rowen',
+            'email' => 'growen@live.com',
+            'password' => Hash::make('@gonzales101')
+        ]);
+    }
+}
